2162a0317c263253bb1e4d94f34621f8a09a2397
Ashley <ashley.ratigan@gmail.com>
2018-07-20 10:24:27 -0400

    Created basic html template
    Added ui, events, and api js files to link api

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const createGame = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/bucketlists/',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+const getGames = function () {
+  return $.ajax({
+    method: 'GET',
+    url: config.apiUrl + '/bucketlists/',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut,
+  createGame,
+  getGames
+}

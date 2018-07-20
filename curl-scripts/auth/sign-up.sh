e05aee4ffbe0ff4c73290f87f39b09ccd60425cd
Ashley <ashley.ratigan@gmail.com>
2018-07-20 10:35:32 -0400

    Added curl scripts for testing

---
+++ curl-scripts/auth/sign-up.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# EMAIL=ava@bob.com PASSWORD=hannah scripts/auth/sign-up.sh
+
+curl "http://localhost:4741/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo

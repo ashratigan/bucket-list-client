85ae4f09f5b97f27c8cf993502325f7580cb8aca
Ashley <ashley.ratigan@gmail.com>
2018-07-21 15:27:56 -0400

    Changed how bucketlists are displayed

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -59,7 +59,7 @@ const getBukectlistsSuccess = function (data) {
     const taskList = data.bucketlists[i].task.split(',')
     console.log(taskList)
     console.log(`ui task data is ${data.bucketlists[i].task}`)
-    $('.displayedBucketlists').append('<p><i>ID:' + data.bucketlists[i].id + '</i><br/><b>NAME:</b>' + data.bucketlists[i].bl_name + '    <br/>' + taskList + '<br/></p><br/>')
+    $('.displayedBucketlists').append('<p><i>ID:' + data.bucketlists[i].id + '</i><br/><b>' + data.bucketlists[i].bl_name + '</b><br/>' + taskList + '<br/></p><br/>')
   }
   console.log(`ui data is ${data}`)
   console.log(`ui data is ${data.bucketlists}`)

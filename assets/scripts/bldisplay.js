4ccc8b507898a1ce1b332fe694f2ccb79d2f6bb5
Ashley <ashley.ratigan@gmail.com>
2018-07-21 15:37:28 -0400

    Cleaned up code

--- assets/scripts/bldisplay.js
+++ assets/scripts/bldisplay.js
@@ -1,26 +1,26 @@
-//Check off specific todos by clicking
+// Check off specific todos by clicking
 $('ul').on('click', 'li', function () {
-    $(this).toggleClass('completed');
-});
-
-//Click on X to delete todo
-$('ul').on('click', 'span', function (event) {
-    $(this).parent().fadeOut(500, function () {
-        $(this).remove();
-    })
-    event.stopPropagation();
+  $(this).toggleClass('completed')
 })
 
-//add event listener to add todo
-$('input[type='text']').keypress(function (event) {
-    if (event.which === 13) {
-        var todoText = $(this).val();
-        $(this).val('');
-        //create new li and add to ul
-        $('ul').append('<li>' + todoText + '</li>')
-    }
-});
+// Click on X to delete todo
+$("ul").on("click", "span", function (event) {
+  $(this).parent().fadeOut(500, function () {
+    $(this).remove()	
+  })
+  event.stopPropagation()
+})
+
+// add event listener to add todo
+$("input[type='text']").keypress(function (event) {
+  if (event.which === 13) {
+    const todoText = $(this).val()
+    $(this).val('')
+    // create new li and add to ul
+    $('ul').append('<li>' + todoText + '</li>')
+  }
+})
 
 $('h1').click(function () {
-    $('input[type='text']').fadeToggle();
-});
+  $("input[type='text']").fadeToggle()
+})

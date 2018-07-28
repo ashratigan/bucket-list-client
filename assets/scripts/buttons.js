1864ca156df80eb575188e5a0aaaf773f8d172b7
Ashley <ashley.ratigan@gmail.com>
2018-07-28 15:38:31 -0400

    Removed butons that were no longer being used

--- assets/scripts/buttons.js
+++ assets/scripts/buttons.js
@@ -4,33 +4,3 @@ $(document).ready(function () {
     $('.userInfo').toggle()
   })
 })
-
-// // function to hide bucketlists
-// $(document).ready(function () {
-//   $('#hideBucketlists').click(function () {
-//     $('.bldisplay').css('display', 'none')
-//   })
-// })
-
-// // function to see bucketlists
-// $(document).ready(function () {
-//   $('#seeeBucketlists').click(function () {
-//     $('.bldisplay').css('display', 'block')
-//   })
-// })
-
-// Function to toggle change password
-$(document).ready(function () {
-  $('#changePasswordButton').click(function () {
-    $('#change-password-form').toggle()
-    $()
-  })
-})
-
-// Function to clear user sign in messages
-$(document).ready(function () {
-  $('#newGame').click(function () {
-    $('#userMessage').text('')
-    $('#userMessage').css('background-color', '222')
-  })
-})
